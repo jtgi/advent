6d382c96156a83d1d01fec7f5c5d7269cdc76d6a
jgiannakos <jgiannakos@twilio.com>
2020-11-18 11:23:22 -0800

    countdown timer

--- public/javascripts/advent.js
+++ public/javascripts/advent.js
@@ -1,7 +1,11 @@
 
 window.addEventListener('load', () => {
+
     if (document.getElementsByClassName('ready').length) {
         $('#loader-wrap').fadeOut('fast');
+    } else {
+        const until = new Date(2020, 11, 1, 0, 0, 0, 0);
+        $('#countdown').countdown({ until });
     }
 
     $([document.documentElement, document.body]).animate({
